a9b332fc51666ce9ff75c35a5dfe09437fef77b3
Pulkitmittal123 <pulkitmittal989898@gmail.com>
2026-04-18 21:23:51 +0530

    Update code

--- frontend/src/components/items.jsx
+++ frontend/src/components/items.jsx
@@ -9,17 +9,20 @@ const Items = () => {
       <div className="items">
         <div className="inner-container">
           <div className="item-card">
-            {loading ? (
-              <div className="spinner">
-                <HashLoader color="#7cc242" />
-              </div>
-            ) : items.length > 0 ? (
-              items.map((item) => (
-                <Card item={item} key={item._id || item.name} />
-              ))
-            ) : (
-              <h1 className="spinner">No Item</h1>
-            )}
+            {
+              loading ?
+                <div className="spinner">
+                  <HashLoader color="#ff7700" />
+                </div>
+                :
+
+                items.length > 0 ? (
+                  items.map((item) => (
+                    <Card item={item} key={item._id || item.name} />
+                  ))
+                ) : (
+                  <h1 className="empty">No Item</h1>
+                )}
           </div>
         </div>
       </div>
